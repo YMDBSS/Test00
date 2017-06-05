71184ba43a27ebb135c2e9995a80f5113f8ae9ab
YMDBSS <yauhen.minko@deutsche-boerse.com>
2017-06-05 17:04:28 +0200

    Added XSD validation logic. Added sample XML and XSD to validate against.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Schema;
 
 namespace Test00
 {
@@ -6,7 +10,74 @@ namespace Test00
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+
+            IDictionary<string, string> schemaMapping = new Dictionary<string, string>();
+
+            schemaMapping.Add("xsd/REMITTable1_V2.xsd", "http://www.acer.europa.eu/REMIT/REMITTable1_V2.xsd");
+            schemaMapping.Add("xsd/REMITTable2_V1.xsd", "http://www.acer.europa.eu/REMIT/REMITTable2_V1.xsd");
+
+            ValidateFileAgainstSchema("xml/RemitTable1.xml", schemaMapping);
+
+            Console.WriteLine("==============End of XML file validation===============");
+
+
+            ValidateFileAgainstSchema("xml/RemitTable2.xml", schemaMapping);
+
+
+
+            Console.ReadLine();
+
+
         }
+
+
+
+        private static void ValidateFileAgainstSchema(string pathToFile, IDictionary<string, string> schemaMapping)
+        {
+
+            foreach (var mappingItem in schemaMapping)
+            {
+
+
+                Console.WriteLine($"Started validation {pathToFile} against {mappingItem.Key}");
+
+                try
+                {
+                    using (var reader = XmlReader.Create(pathToFile))
+                    {
+                        XmlSchemaSet xsdSchema = new XmlSchemaSet();
+                        xsdSchema.Add(mappingItem.Value, mappingItem.Key);
+                        XDocument doc = XDocument.Load(reader);
+
+                        doc.Validate(xsdSchema, ValidationCompleted);
+
+
+
+                    }
+                }
+                catch (Exception e)
+                {
+
+                    Console.WriteLine(e.Message);
+                }
+
+                Console.WriteLine($"Finished validation {pathToFile} against {mappingItem.Key}");
+
+            }
+
+
+        }
+
+        private static void ValidationCompleted(object sender, ValidationEventArgs e)
+        {
+
+            if (e.Exception != null)
+                Console.WriteLine($"Validation status {e.Severity}, exception - { e.Exception}");
+            else
+                Console.WriteLine($"Validation status {e.Severity}, info message - {e.Message}");
+        }
+
+
     }
 }
